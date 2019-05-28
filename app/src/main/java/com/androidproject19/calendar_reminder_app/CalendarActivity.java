43a75acb75b0793c244811d53e75e0715427dca9
SpicyF1sh <haydensand011@gmail.com>
2019-05-28 10:08:27 -0400

    xml update - Hayden

--- app/src/main/java/com/androidproject19/calendar_reminder_app/CalendarActivity.java
+++ app/src/main/java/com/androidproject19/calendar_reminder_app/CalendarActivity.java
@@ -1,19 +1,29 @@
 package com.androidproject19.calendar_reminder_app;
 
-import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class CalendarActivity extends AppCompatActivity {
+    private static final String TAG = "CalendarActivity";
+    private CalendarView mCalendarView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_view);
-    }
-    private void configureEventButton (){
-        startActivity(new Intent(this,Event.class));
+        mCalendarView = (CalendarView) findViewById(R.id.calendarView);
+
+        mCalendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView calendarView, int year, int month, int dayOfMonth) {
+                String date = dayOfMonth + "/" + month + "/" + year;
+                Log.d(TAG, "onSelectedDayChange: date; " + date);
+
             }
+        });
+    }
+
 }
 
