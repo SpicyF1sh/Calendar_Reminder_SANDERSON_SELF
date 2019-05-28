20fe40634554d4d6d065aeb378c3f590ae8bc3c1
SpicyF1sh <haydensand011@gmail.com>
2019-05-28 10:07:21 -0400

    xml update - Hayden

--- app/src/main/java/com/androidproject19/calendar_reminder_app/CalendarActivity.java
+++ app/src/main/java/com/androidproject19/calendar_reminder_app/CalendarActivity.java
@@ -1,7 +1,10 @@
 package com.androidproject19.calendar_reminder_app;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class CalendarActivity extends AppCompatActivity {
     @Override
@@ -9,4 +12,8 @@ public class CalendarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.calendar_view);
     }
+    private void configureEventButton (){
+        startActivity(new Intent(this,Event.class));
+            }
 }
+
---
+++ app/src/main/java/com/androidproject19/calendar_reminder_app/UserStats.java
@@ -0,0 +1,6 @@
+package com.androidproject19.calendar_reminder_app;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class UserStats extends AppCompatActivity {
+}
